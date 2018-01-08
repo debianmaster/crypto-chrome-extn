95471aacb29879c9cc4ee1166fc0e8c2c8a81ae8
Chakradhar Jonagam <9chakri@gmail.com>
2018-01-07 17:55:29 -0800

    update

--- src/bg/cryptoprice.js
+++ src/bg/cryptoprice.js
@@ -8,14 +8,36 @@ var app = new Vue({
   el: '#app',
   data() {
     return {
-      coins: []
+      coins: [],
+      totalUSDBalance:0,
+      qty:{
+        "DCN":200000.00000000,
+        "TRX":42399.86720300, 
+        "XVG":23447.89800000, 
+        "VOISE":1918.78531541, 
+        "PAC":20863549.40119760,
+        "MINT":0,
+        "DOGE":85182.60146374,
+        "RDD":47633.61019896,
+        "KIN":3863398.244,
+        "DOT":3550.77932768,
+        "BTC":0.06786401,
+        "MANA":0,
+        "XLM":0.30268000,
+        "XRP":26.32200000,
+        "IOTA":0.24200000,
+        "SC":0,
+        "DENT":0,
+        "ETH":3.75881222,
+        "LINDS":132801.08410630
+      }
     }
   },
   computed: {
     filterCoins: function() {
-      var symbols = ["DCN","TRX", "XVG", "VOISE", "PAC", "MINT", "DOGE","RDD","KIN","DOT","VOISE","BTC","MANA","XLM","XRP","IOTA","SC","DENT"];
+      var symbols = ["DCN","TRX", "XVG", "VOISE", "PAC", "MINT", "DOGE","RDD","KIN","DOT","BTC","MANA","XLM","XRP","IOTA","SC","DENT"];
       return this.coins.filter(function(coin){
-        return symbols.indexOf(coin.symbol) > -1 
+        return symbols.indexOf(coin.symbol) > -1;
       });
     }
   },
@@ -31,10 +53,19 @@ var app = new Vue({
                 response.status);
               return;
             }
-
             // Examine the text in the response
             response.json().then(function(coins) {
+              var totalBalance=0;
+              coins.map(function(coin,index){
+                  coin['qty']=self.qty[coin.symbol];
+                  coin['usdValue']=parseFloat(coin['qty']*coin.price_usd).toFixed(0);
+                  if(!isNaN(coin['usdValue'])){
+                  totalBalance+=parseFloat(coin['usdValue']);
+                  coin.price_usd=parseFloat(coin.price_usd).toFixed(4);
+                  }
+              });
               self.coins = coins;
+              self.totalUSDBalance=totalBalance;
             });
           }
         )
@@ -47,5 +78,5 @@ var app = new Vue({
 app.fetchUpdates();
 setInterval(function(){
   //console.log('test')
-  app.fetchUpdates();
+  //app.fetchUpdates();
 },60000)
