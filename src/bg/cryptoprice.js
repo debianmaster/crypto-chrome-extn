fdf7d03c5aac78c38ddbd39082099eb169640672
Chakradhar Jonagam <9chakri@gmail.com>
2018-01-07 21:31:33 -0800

    update

--- src/bg/cryptoprice.js
+++ src/bg/cryptoprice.js
@@ -16,28 +16,23 @@ var app = new Vue({
         "XVG":23447.89800000, 
         "VOISE":1918.78531541, 
         "PAC":20863549.40119760,
-        "MINT":0,
-        "DOGE":85182.60146374,
+        "BCN":332300.00000000,
+        "DOGE":(85182.60146374+49965.557622),
         "RDD":47633.61019896,
         "KIN":3863398.244,
         "DOT":3550.77932768,
-        "BTC":0.06786401,
-        "MANA":0,
-        "XLM":0.30268000,
         "XRP":26.32200000,
-        "IOTA":0.24200000,
-        "SC":0,
-        "DENT":0,
-        "ETH":3.75881222,
-        "LINDS":132801.08410630
+        "FUEL":2061.876,
+        "ETH":0.00419644,
+        "LINDA":132801.08410630
       }
     }
   },
   computed: {
-    filterCoins: function() {
-      var symbols = ["DCN","TRX", "XVG", "VOISE", "PAC", "MINT", "DOGE","RDD","KIN","DOT","BTC","MANA","XLM","XRP","IOTA","SC","DENT"];
+    filterCoins: function() { 
+      var that=this; 
       return this.coins.filter(function(coin){
-        return symbols.indexOf(coin.symbol) > -1;
+        return (undefined!==that.qty[coin.symbol])
       });
     }
   },
@@ -78,5 +73,5 @@ var app = new Vue({
 app.fetchUpdates();
 setInterval(function(){
   //console.log('test')
-  //app.fetchUpdates();
+  app.fetchUpdates();
 },60000)
